c13ff0e6b167089f1380648915de344a0bfa6bbd
Nervanin <Volua53@yandex.ru>
2019-09-08 10:31:26 +0300

    pod's init

---
+++ SearchTestApp/SearchTestApp/CatalogDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  CatalogDataSource.swift
+//  SearchTestApp
+//
+//  Created by Konstantin on 08/09/2019.
+//  Copyright © 2019 Konstantin Meleshko. All rights reserved.
+//
+
+import UIKit
+
+class CatalogDataSource: UICollectionViewDataSource {
+    
+}
