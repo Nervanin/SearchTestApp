9370a560ce88b968289a6583ccc0fd65900e8214
Nervanin <Volua53@yandex.ru>
2019-09-08 10:15:27 +0300

    no message

---
+++ SearchTestApp/SearchTestApp/CatalogViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchTestApp
+//
+//  Created by Konstantin on 08/09/2019.
+//  Copyright © 2019 Konstantin Meleshko. All rights reserved.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
